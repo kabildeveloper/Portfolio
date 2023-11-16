0b079555003fac4d42b9ab39c3f3b1f0c2d5da67
kabildeveloper <kabildeveloper@gmail.com>
2023-11-16 10:12:18 +0530

    Added title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,7 @@ import {Head, Html, Main, NextScript} from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
+            <title> Kabil Rajendhiran </title>
             <Head/>
             <body>
             <Main/>
